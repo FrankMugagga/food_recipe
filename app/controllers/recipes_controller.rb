c0ebd622201b57df6ecbc853484f09b9653f0548
FrankMugagga <frank.mugagga@gmail.com>
2024-04-19 17:14:50 +0300

    Correct the update method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,29 +8,48 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @user = User.find(params[:id])
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.new
   end
 
   def create
-    @user = User.find(params[:id])
-    @recipe = Recipe.new(recipe_params)
+    @recipe = current_user.recipes.new(recipe_params)
     if @recipe.save
-      redirect_to @recipe, notice: 'Recipe was successfull created'
+      redirect_to recipes_path, notice: 'Recipe was successfull created'
     else
       render :new
     end
   end
 
+  def edit
+    @recipe = current_user.recipes.find(params[:id])
+  end
+
+  def update
+    @recipe = current_user.recipes.find(params[:id])
+
+    if @recipe.update(recipe_params)
+      redirect_to recipes_path, notice: 'Recipe was successfully update'
+    else
+      render :edit
+    end
+  end
+  
   def destroy
-    @recipe = recipe.find(params[:id])
-    @inventory.destroy
-    redirect_to recipes_url, notice: 'Inventory was successfully destroyed'
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_url, notice: 'Recipe was successfully destroyed'
+  end
+
+  def toggle_public
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(public: !@recipe.public)
+    redirect_back(fallback_location: recipe_path)
   end
 
   private
 
   def recipe_params
-    params.required(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+    params.required(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public)
   end
 end
+
