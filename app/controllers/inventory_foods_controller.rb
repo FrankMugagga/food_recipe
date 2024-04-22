2d4831eedfa278f674c2e1adfb9b024f0d63a161
FrankMugagga <frank.mugagga@gmail.com>
2024-04-22 13:43:42 +0300

    Set actions for instance variables

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,35 +1,48 @@
 class InventoryFoodsController < ApplicationController
+  before_action :set_inventory
+  before_action :set_inventory_food, only: [:destroy]
+
   def index
-    @inventory_foods = InventroyFood.all
+    @inventory_foods = InventoryFood.all   
   end
 
   def show
-    @inventory_food = InventroyFood.find(params[:id])
+    @inventory_food = InventoryFood.find(params[:id])    
   end
 
   def new
-    @inventory_food = InventoryFood.new
+    @inventory_food = @inventory.inventory_foods.new
   end
 
   def create
-    @inventory_food = InventoryFood.new(inventory_food_params)
-
+    @inventory_food = @inventory.inventory_foods.build(inventory_food_params)
     if @inventory_food.save
-      redirect_to @inventory_food, notice: 'Inventory food was successfully created'
+      redirect_to inventory_path(@inventory), notice: 'Food was successfully create'
     else
       render :new
     end
   end
 
   def destroy
-    @inventory_food = InventoryFood.find(params[:id])
     @inventory_food.destroy
-    redirect_to inventory_foods_url, notice: 'Inventory food was successfully destroyed'
+    redirect_to inventory_path(@inventory), notice: 'Inventory was successfully deleted'
   end
-
+  
   private
 
-  def inventory_food_params
-    params.require(:inventory_food).permit(:quantity, :inventory_id, :food_id)
+  def set_inventory
+    @inventory = current_user.inventories.find_by(id: params[:inventory_id])
+    unless @inventory
+      redirect_to inventories_path, alert: 'Inventory not found'
+    end
   end
+
+  def set_inventory_food
+    @inventory_food = @inventory.inventory_foods.find(params[:id])
+  end
+
+  def inventory_food_params
+    params.require(:inventory_food).permit(:food_id, :quantity)
+  end
+
 end
