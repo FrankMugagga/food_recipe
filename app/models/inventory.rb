27dcc1a24ed4d39f15234d1a13405a4d29923b55
FrankMugagga <frank.mugagga@gmail.com>
2024-04-28 14:07:51 +0300

    Add relationship with shopping list

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,5 @@ class Inventory < ApplicationRecord
   belongs_to :user
   has_many :inventory_foods
   has_many :foods, through: :inventory_foods
+  has_many :shopping_lists
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :foods, through: :recipe_foods
+  has_many :shopping_lists
 end
