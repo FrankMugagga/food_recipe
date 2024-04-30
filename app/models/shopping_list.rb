d369560d0d4e79354eafb517180d89dd63c12837
FrankMugagga <frank.mugagga@gmail.com>
2024-04-30 21:24:10 +0300

    Refactor code to avoid errors

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -5,32 +5,28 @@ class ShoppingList < ApplicationRecord
   def missing_items
     return @missing_items if defined?(@missing_items)
 
-    @missing_items = []
-    recipe.foods.includes(:recipe_foods, :inventory_foods).each do |food|
-      recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
-      inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
-      recipe_amount = recipe_food.quantity
-      inventory_amount = inventory_food&.quantity || 0
-
-      if inventory_amount < recipe_amount
-        missing_quantity = recipe_amount - inventory_amount
-        @missing_items << {
-          food: food.name,
-          required: recipe_amount,
-          missing: missing_quantity,
-          price: food.price 
-        }
-      end
-    end
+    @missing_items = recipe.foods.includes(:recipe_foods, :inventory_foods).map do |food|
+      calculate_missing_items_for_food(food)
+    end.compact
     @missing_items
   end
 
-  def total_price
-    missing_items.sum { |item| item[:price] }
-  end
+  private
 
-  def total_missing_items
-    missing_items.count
-  end
+  def calculate_missing_items_for_food(food)
+    recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
+    inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
+    recipe_amount = recipe_food.quantity
+    inventory_amount = inventory_food&.quantity || 0
 
+    return if inventory_amount >= recipe_amount
+
+    missing_quantity = recipe_amount - inventory_amount
+    {
+      food: food.name,
+      required: recipe_amount,
+      missing: missing_quantity,
+      price: food.price
+    }
+  end
 end
