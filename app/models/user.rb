2387a8e84c4cec6f9a0c4e4dda9629b773aeb62d
FrankMugagga <frank.mugagga@gmail.com>
2024-05-01 22:49:57 +0300

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   has_many :recipes
   has_many :foods
   has_many :inventories
+
+  validates :name, :email, :password, :password_confirmation, presence: { message: "missing value, cant't be blank" }
+  
+  validate :password_complexity
+
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[\W]).{8,70}$/
+    errors.add :password, 'password complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
