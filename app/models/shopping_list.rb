15c253ea199a2ecafecd09f72eb6d7158d205353
FrankMugagga <frank.mugagga@gmail.com>
2024-05-01 22:50:52 +0300

    Change link name to generate shopping list

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -5,28 +5,31 @@ class ShoppingList < ApplicationRecord
   def missing_items
     return @missing_items if defined?(@missing_items)
 
-    @missing_items = recipe.foods.includes(:recipe_foods, :inventory_foods).map do |food|
-      calculate_missing_items_for_food(food)
-    end.compact
+    @missing_items = []
+    recipe.foods.includes(:recipe_foods, :inventory_foods).each do |food|
+      recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
+      inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
+      recipe_amount = recipe_food.quantity
+      inventory_amount = inventory_food&.quantity || 0
+
+      next unless inventory_amount < recipe_amount
+
+      missing_quantity = recipe_amount - inventory_amount
+      @missing_items << {
+        food: food.name,
+        required: recipe_amount,
+        missing: missing_quantity,
+        price: food.price
+      }
+    end
     @missing_items
   end
 
-  private
+  def total_price
+    missing_items.sum { |item| item[:price] }
+  end
 
-  def calculate_missing_items_for_food(food)
-    recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
-    inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
-    recipe_amount = recipe_food.quantity
-    inventory_amount = inventory_food&.quantity || 0
-
-    return if inventory_amount >= recipe_amount
-
-    missing_quantity = recipe_amount - inventory_amount
-    {
-      food: food.name,
-      required: recipe_amount,
-      missing: missing_quantity,
-      price: food.price
-    }
+  def total_missing_items
+    missing_items.count
   end
 end
