9add82336077f08db5cd09b151f95325a47710ad
FrankMugagga <frank.mugagga@gmail.com>
2024-05-02 22:54:14 +0300

    Refactor to remove cyclomatic complexity

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -4,26 +4,33 @@ class ShoppingList < ApplicationRecord
 
   def missing_items
     return @missing_items if defined?(@missing_items)
-
-    @missing_items = []
-    recipe.foods.includes(:recipe_foods, :inventory_foods).each do |food|
-      recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
-      inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
-      recipe_amount = recipe_food.quantity
-      inventory_amount = inventory_food&.quantity || 0
-
-      next unless inventory_amount < recipe_amount
-
-      missing_quantity = recipe_amount - inventory_amount
-      @missing_items << {
-        food: food.name,
-        required: recipe_amount,
-        missing: missing_quantity,
-        price: food.price
-      }
-    end
+    
+    @missing_items = recipe.foods.includes(:recipe_foods, :inventory_foods).map do |food|
+      item_details(food)
+    end.compact
     @missing_items
   end
+
+  private
+
+  def item_details(food)
+    recipe_food = food.recipe_foods.find { |rf| rf.recipe_id == recipe.id }
+    inventory_food = food.inventory_foods.find { |ifood| ifood.inventory_id == inventory.id }
+    recipe_amount = recipe_food.quantity
+    inventory_amount = inventory_food&.quantity || 0
+
+    return nil if inventory_amount >= recipe_amount
+
+    missing_quantity = recipe_amount - inventory_amount
+    {
+      food: food.name,
+      required: recipe_amount,
+      missing: missing_quantity,
+      price: food.price
+    }
+  end
+
+  public
 
   def total_price
     missing_items.sum { |item| item[:price] }
