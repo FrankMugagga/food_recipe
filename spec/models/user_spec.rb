be17cfcf0224928241e826515dccfdf3cf29d5fe
FrankMugagga <frank.mugagga@gmail.com>
2024-05-04 00:35:38 +0300

    Test user validity

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+  end
+
+  describe 'Validates presence of parameters' do
+    it 'Validates the user input' do
+      expect(@user).to be_valid
+    end
+  end
 end
