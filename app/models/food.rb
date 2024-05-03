a2283972527316c280f5562395f0c10ddc4d8ded
FrankMugagga <frank.mugagga@gmail.com>
2024-05-04 00:34:16 +0300

    Add dependent destroy

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,9 +1,11 @@
+
 class Food < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :recipes, through: :recipe_foods
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
   has_many :inventories, through: :inventory_foods
 
-  validate :name, :measurement_unit, :price, presence: { message: 'Plaese enter value, Cannot be blank' }
+  validates :name, :measurement_unit, :price, presence: { message: 'Please enter value, Cannot be blank' } # Corrected typo in 'Please'
 end
+
