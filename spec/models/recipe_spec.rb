edf6f7e33e3f7ca9535e55d0fd123d4876003e54
FrankMugagga <frank.mugagga@gmail.com>
2024-05-05 00:17:52 +0300

    Test for the validity with all variables input

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+  end
+
+  describe 'Valide recipe' do
+    it 'Validates the creation of a recipe' do
+      @recipe = @user.recipes.create( user_id: @user.id, name: 'recipe one', preparation_time: 2.5, cooking_time: 3, description: 'recipe one is the very best', public: true )
+      expect( @recipe).to be_valid
+    end
+  end
 end
