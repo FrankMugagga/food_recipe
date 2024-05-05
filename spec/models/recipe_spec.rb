d05527e18d460d4b0531a2800fa7c380b2cc7bbc
FrankMugagga <frank.mugagga@gmail.com>
2024-05-05 06:46:54 +0300

    test resource validity, and relationships

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -4,12 +4,37 @@ RSpec.describe Recipe, type: :model do
   before do
     @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
     @user.confirm
+    @recipe = @user.recipes.create( user_id: @user.id, name: 'recipe one', preparation_time: 2.5, cooking_time: 3, description: 'recipe one is the very best', public: true )
   end
 
-  describe 'Valide recipe' do
-    it 'Validates the creation of a recipe' do
-      @recipe = @user.recipes.create( user_id: @user.id, name: 'recipe one', preparation_time: 2.5, cooking_time: 3, description: 'recipe one is the very best', public: true )
+  describe 'Validity of a  recipe' do
+    it 'is valid if the fields are filled' do
       expect( @recipe).to be_valid
+    end
+
+    it 'validates the presence of each field' do
+      is_expected.to validate_presence_of(:name).with_message('Field cannot be blank')
+      is_expected.to validate_presence_of(:preparation_time).with_message('Field cannot be blank')
+      is_expected.to validate_presence_of(:cooking_time).with_message('Field cannot be blank')
+      is_expected.to validate_presence_of(:description).with_message('Field cannot be blank')
+    end
+  end
+
+  describe 'the recipe relationships' do
+    it 'belongs to a user' do
+      expect(@recipe).to belong_to(:user)
+    end
+
+    it 'has many recipe_foods' do
+      expect(@recipe).to have_many(:recipe_foods).dependent(:destroy)
+    end
+
+    it 'has one shopping' do
+      expect(@recipe).to have_one(:shopping_list).dependent(:destroy)
+    end
+
+    it 'is expected to have many foods through recipe_foods' do
+      expect(@recipe).to have_many(:foods).through(:recipe_foods)
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,89 @@ RSpec.describe User, type: :model do
     @user.confirm
   end
 
-  describe 'Validates presence of parameters' do
-    it 'Validates the user input' do
+  describe 'The user validity' do
+    it 'is valid if all attributes are filled' do
       expect(@user).to be_valid
+    end
+
+    it 'validates the presence of each field' do
+      is_expected.to validate_presence_of(:name).with_message("missing value, can't be blank")
+      is_expected.to validate_presence_of(:email).with_message("missing value, can't be blank")
+      is_expected.to validate_presence_of(:password).with_message("missing value, can't be blank")
+      is_expected.to validate_presence_of(:password_confirmation).with_message("missing value, can't be blank")
+    end
+
+    it 'displays an error message if a value is missing' do
+      @user.name = ''
+      @user.valid?
+      expect(@user).to be_invalid
+      expect(@user.errors[:name]).to include("missing value, can't be blank")
+    end
+
+    it 'is invalid with a password that is too short' do
+      @user.password = 'As@123'
+      @user.password_confirmation = 'As@123'
+      @user.valid?
+      expected_message = 'password complexity requirement not met.' +
+      'Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+      expect(@user.errors[:password]).to include(expected_message)
+    end
+
+    it 'is invalid without an uppercase letter' do
+      @user.password = 'as@1234567'
+      @user.password_confirmation = 'as@1234567'
+      @user.valid?
+      expected_message = 'password complexity requirement not met.' +
+      'Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+      expect(@user.errors[:password]).to include(expected_message)
+    end
+
+    it 'is invalid without a lowercase letter' do
+      @user.password = 'AS@1234567'
+      @user.password_confirmation = 'AS@1234567'
+      @user.valid?
+      expected_message = 'password complexity requirement not met.' +
+      'Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+      expect(@user.errors[:password]).to include(expected_message)
+    end
+
+    it 'is invalid without a digit' do
+      @user.password = 'AS@asdfgh'
+      @user.password_confirmation = 'AS@asdfghj'
+      @user.valid?
+      expected_message = 'password complexity requirement not met.' +
+      'Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+      expect(@user.errors[:password]).to include(expected_message)
+    end
+
+    it 'is invalid without a special character' do
+      @user.password = 'AS0asdfgh'
+      @user.password_confirmation = 'AS0asdfghj'
+      @user.valid?
+      expected_message = 'password complexity requirement not met.' +
+      'Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+      expect(@user.errors[:password]).to include(expected_message)
+    end
+
+    it 'is invalid with if the one email add is used by different users' do
+      @user1 = User.create(name: 'bod', email: 'zed@g.com', password: '1234567Y#r', password_confirmation: '1234567Y#r')
+      expect(@user1).to be_invalid
+      expect(@user1.errors[:email]).to include('has already been taken')
+    end
+
+  end
+
+  describe 'the user relationships' do
+    it 'has many recipes' do
+      expect(@user).to have_many(:recipes).dependent(:destroy)
+    end
+
+    it 'has many foods' do
+      expect(@user).to have_many(:foods).dependent(:destroy)
+    end
+
+    it 'has many inventories' do
+      expect(@user).to have_many(:inventories).dependent(:destroy)
     end
   end
 end
