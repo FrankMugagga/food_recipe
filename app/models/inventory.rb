cbf6902d3247b3f114c36a2b610d1f5197432f82
FrankMugagga <frank.mugagga@gmail.com>
2024-05-05 23:37:00 +0300

    Add dependent destroy'

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,9 +1,8 @@
 class Inventory < ApplicationRecord
   belongs_to :user
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods
-  has_many :shopping_lists
+  has_many :shopping_lists, dependent: :destroy
 
-  validates :name, description presence: true
-
+  validates :name, :description, presence: { message: 'cannot be blank' }  
 end
