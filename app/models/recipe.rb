2e0bce988a31797d27aa4bc8c048efd7f34eba98
FrankMugagga <frank.mugagga@gmail.com>
2024-05-05 06:44:52 +0300

    Add dependent destroy

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :recipe_foods
+  has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
-  has_many :shopping_lists
+  has_one :shopping_list, dependent: :destroy
 
-  validates :name, :preparation_time, :cooking_time, :description, :public, presence: true
+  validates :name, :preparation_time, :cooking_time, :description, presence: { message: 'Field cannot be blank' }
 end
