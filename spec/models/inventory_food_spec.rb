7bd21b1ae683bc05dfa79c555f9edb38f93e3bcf
FrankMugagga <frank.mugagga@gmail.com>
2024-05-05 23:38:18 +0300

    Test the relationships and attribute validity

--- spec/models/inventory_food_spec.rb
+++ spec/models/inventory_food_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe InventoryFood, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+    @food = @user.foods.create(user_id: @user.id,name: 'cassava', measurement_unit: 'kilogram', price: 2000)
+    @inventory = @user.inventories.create(name: 'inventory one', user_id: @user.id, description: 'This inventory ....')
+    @inventory_food = InventoryFood.create( inventory_id: @inventory.id, food_id: @food.id, quantity: 80  )
+  end
+
+  describe 'Validity of the inventory_food' do
+    it 'is valid if the field is filled' do
+      expect(@inventory_food).to be_valid
+    end
+
+    it 'validates the presence of the quantity' do
+      is_expected.to validate_presence_of(:quantity).with_message('Cannot be blank')
+    end
+  end
+
+  describe 'Validity of the realtionship' do
+    it 'belongs to inventory' do
+      expect(@inventory_food).to belong_to(:inventory)
+    end
+
+    it 'belongs to food' do
+      expect(@inventory_food).to belong_to(:food)
+    end
+  end
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Inventory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+    @inventory = @user.inventories.create(name: 'inventory one', user_id: @user.id, description: 'This inventory ....')
+  end
+
+  describe 'the inventory validity' do
+    it 'is valid if all values are filed' do
+      expect(@inventory).to be_valid
+    end
+
+    it 'validates the presence of each field' do
+      is_expected.to validate_presence_of(:name).with_message( 'cannot be blank' )
+      is_expected.to validate_presence_of(:description).with_message( 'cannot be blank' )
+    end
+  end
+
+  describe 'the inventory relationships' do
+    it 'belongs to the user' do
+      expect(@inventory).to belong_to(:user)
+    end
+
+    it 'has many inventory_foods' do
+      expect(@inventory).to have_many(:inventory_foods)
+    end
+
+    it 'has many foods through inventory foods' do
+      expect(@inventory).to have_many(:foods).through(:inventory_foods)
+    end
+
+    it '' do
+    end
+  end
 end
