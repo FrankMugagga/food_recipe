982ed4e6e29c6d39ba1b8e2758b94d74e959adcf
FrankMugagga <frank.mugagga@gmail.com>
2024-05-07 23:42:32 +0300

    Test the controller requets functionality

--- spec/requests/public_recipes_spec.rb
+++ spec/requests/public_recipes_spec.rb
@@ -1,10 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'PublicRecipes', type: :request do
+  include Devise::Test::IntegrationHelpers
+
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+    expect(@user.confirmed?).to be true
+    sign_in @user
+  end
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/public_recipes/index'
+      get '/public_recipes'
       expect(response).to have_http_status(:success)
+    end
+
+    it 'renders the index template' do
+      get '/public_recipes'
+      expect(response).to render_template(:index)
+    end
+
+    it 'includes "@user_food_count "' do
+      get '/public_recipes'
+      expect(response.body).to include(@user_food_count.to_s)
     end
   end
 end
