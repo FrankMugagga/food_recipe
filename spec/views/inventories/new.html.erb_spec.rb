12dd398fc73159076dd9a82d74b0581d6ddfbe50
FrankMugagga <frank.mugagga@gmail.com>
2024-05-14 00:37:17 +0300

    Test the inventory view methods

--- spec/views/inventories/new.html.erb_spec.rb
+++ spec/views/inventories/new.html.erb_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'inventories/new,.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+  include Capybara::DSL
+
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+    expect(@user.confirmed?).to be true
+    sign_in @user
+       
+    visit new_inventory_path
+  end
+
+  describe 'inventory new page' do
+    it 'will render inventory new page' do
+      expect(page).to have_content( 'Create Inventory' )
+      expect(page).to have_button('Create inventory')    
+      expect(page).to have_field(@inventory_name)  
+    end
+
+    it 'redirects to the user index' do
+      click_link 'Back'
+      expect(page.current_path).to eq(inventories_path)
+    end
+  end
 end
--- spec/views/inventories/show.html.erb_spec.rb
+++ spec/views/inventories/show.html.erb_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'inventories/show,.html.erb', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include Devise::Test::IntegrationHelpers
+  include Capybara::DSL
+
+  before do
+    @user = User.create(name: 'zed', email: 'zed@g.com', password: 'As@1234567', password_confirmation: 'As@1234567')
+    @user.confirm
+    expect(@user.confirmed?).to be true
+    sign_in @user
+    @inventory = @user.inventories.create(name: 'inventory one', user_id: @user.id, description: 'This inventory ....')
+    visit inventory_path(@inventory)
+  end
+
+  describe 'inventory show page' do
+    it 'renders the inventory show page' do
+      expect(page).to have_content('Inventory')
+      expect(page).to have_content( @inventory.name )
+      expect(page).to have_link('Add Food', href: new_inventory_inventory_food_path(@inventory) )
+    end
+
+    it 'redirects to the inventories index' do
+      click_link 'Back'
+      expect(page.current_path).to eq(inventories_path)
+    end
+  end
+
 end
