2fcb9d7d11f185ebeb8a7ec03dd1bd1308c856cc
FrankMugagga <frank.mugagga@gmail.com>
2024-05-16 23:03:36 +0300

    correct relationship

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
   has_many :foods, through: :recipe_foods
-  has_many :shopping_lists, dependent: :destroy
+  has_one :shopping_list, dependent: :destroy
 
   validates :name, :preparation_time, :cooking_time, :description, presence: { message: 'Field cannot be blank' }
 end
