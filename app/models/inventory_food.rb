0fece545dbf16b741944469f765213d0da999dc7
FrankMugagga <frank.mugagga@gmail.com>
2024-06-04 21:27:11 +0300

    Add numericality

--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -3,4 +3,5 @@ class InventoryFood < ApplicationRecord
   belongs_to :food
 
   validates :quantity, presence: { message: 'Cannot be blank' }
+  validates :quantity, numericality: { greater_than_or_equal_to: 0, only_integer: true } 
 end
