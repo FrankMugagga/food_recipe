bd531d3a0fe2063c5b563f45016542ae6acb8f84
FrankMugagga <frank.mugagga@gmail.com>
2024-06-05 15:40:31 +0300

    Remove the confirmable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "food_recipe_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'food-recipe-sa95.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'food-recipe-sa95.onrender.com', protocol: 'https' }
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
